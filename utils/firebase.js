c324ebc7ec50d9b580857ebc27f3b582ffc9aa8d
VMR21 <reddymodith@gmail.com>
2025-04-25 19:18:59 +0530

    Update firebase.js

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCi_YgKl672ZOM5U7zgJxs-uz9KUXj_J9M",
+  authDomain: "tryfirst-fba16.firebaseapp.com",
+  projectId: "tryfirst-fba16",
+  storageBucket: "tryfirst-fba16.firebasestorage.app",
+  messagingSenderId: "457536938894",
+  appId: "1:457536938894:web:b89b7a50f7972f852f3adf"
 };
 
 export const app = initializeApp(firebaseConfig);
