b87c62ea5d710f45e3f2367c7d423a44cfba7e53
VMR21 <reddymodith@gmail.com>
2025-05-05 23:46:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
-import { db } from "@/firebase";
+import { db } from '../firebase'
 import Link from "next/link";
 
 export default function Home() {
